4b8f004d58d280b3775b0765513fcb1b2e1b207e
rdorta <rdorta@forus.cl>
2022-04-26 21:49:47 -0400

    Implementación del login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import '../style/global.scss'
+import Layout from '../containers/Layout'
+import Login from '../containers/Login'
+import '../style/global.css'
 
 const App = () => {
     return (
-        <h1>Hola mundo</h1>
+        <Layout >
+            <Login />
+        </Layout>
     )
 }
 
